91726385e4e66b61dd76e2d28a13f088654ce179
Rafael <rafaelsouzavalle@gmail.com>
2022-02-24 17:18:48 -0300

    Crie projeto com Servlet e HTML bases

---
+++ src/main/java/servlet/ProdutoServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ProdutoServlet")
+public class ProdutoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter saida = null;
+		
+		try {
+			saida = response.getWriter();
+			saida.print("Um olá do Servlet!");
+		} finally {
+			saida.close();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
